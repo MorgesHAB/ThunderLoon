6068ee42318fa42b7d0539782ec4fec3b247c397
MorgesHAB <lionel.isoz@gmail.com>
2017-09-17 11:55:27 +0200

    NodeRed+file configuration

---
+++ lora_receiver/KML/KML.sh
@@ -0,0 +1,2 @@
+sudo cp /home/pi/lora_receiver/KML/Balloon.kml /home/pi/lora_receiver/KML/kml_ready_to_ftp/Balloon2.kml
+sudo echo "</coordinates></LineString></Placemark></Document></kml>" >> /home/pi/lora_receiver/KML/kml_ready_to_ftp/Balloon2.kml
---
+++ lora_receiver/KML/KML2.sh
@@ -0,0 +1,2 @@
+sudo cp /home/pi/lora_receiver/KML/Receiver.kml /home/pi/lora_receiver/KML/kml_ready_to_ftp/Receiver2.kml
+sudo echo "</coordinates></LineString></Placemark></Document></kml>" >> /home/pi/lora_receiver/KML/kml_ready_to_ftp/Receiver2.kml
